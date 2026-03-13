058b46bd429a41ccd96a22e7e0296bee19642d95
Black Rose <of123baran@outlook.com>
2026-03-13 03:11:24 +0300

    Update voice_client.py

--- nextcord/voice_client.py
+++ nextcord/voice_client.py
@@ -666,8 +666,6 @@ class VoiceClient(VoiceProtocol):
         opus.OpusError
             Encoding the data failed.
         """
-        if not self.is_playing():
-            return
         self.checked_add("sequence", 1, 65535)
         encoded_data = self.encoder.encode(data, self.encoder.SAMPLES_PER_FRAME) if encode else data
         packet = self._get_voice_packet(encoded_data)
